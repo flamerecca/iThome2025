1699fe42107fe5c9d274f3c0e625cd2e2606371b
recca <flamerecca711@gmail.com>
2025-09-09 22:43:39 +0800

    Add Filament

---
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ProductInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('description')
+                    ->placeholder('-')
+                    ->columnSpanFull(),
+                TextEntry::make('price')
+                    ->money(),
+                TextEntry::make('stock')
+                    ->numeric(),
+                IconEntry::make('is_active')
+                    ->boolean(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('category_id')
+                    ->numeric()
+                    ->placeholder('-'),
+            ]);
+    }
+}
