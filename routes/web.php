050277eacbf97c0c61d7455ccfce0ae5c948e5a1
recca <flamerecca711@gmail.com>
2025-09-21 22:13:51 +0800

    Add Cache

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,29 @@
 <?php
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/books', function () {
+    DB::enableQueryLog();
+    $users = Cache::remember('users_with_books', 600, function () {
+        return User::with('books')->get(); // 避免 N+1
+    });
+    $books = collect();
+    foreach ($users as $user) {
+        $userData = collect();
+        foreach ($user->books as $book) {
+            $userData->push($book->title);
+        }
+        $books->push($user);
+    }
+    return DB::getQueryLog();
+})->name('books');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
